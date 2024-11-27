b2b6f7c60ba1c834bb1489b8cc2c8519483be3d9
Ntoi Korotsoane <korotsoanentoi@gmail.com>
2024-11-27 20:57:54 +0200

    commit dashboard ui

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,65 @@
-const Products = () => {
-    return (
-        <div>Products</div>
-    )
-}
+import Link from "next/link";
+import styles from "../../ui/dashboard/products/products.module.css";
+import Search from "../../ui/dashboard/search/search";
+import Image from "next/image";
+import Pagination from "../../ui/dashboard/pagination/pagination";
 
-export default Products
+const Products = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                Mac ProBook
+              </div>
+            </td>
+            <td>Description</td>
+            <td>R3030</td>
+            <td>13.32.3030</td>
+            <td>14</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={"/dashboard/products/test"}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
+
+export default Products;
