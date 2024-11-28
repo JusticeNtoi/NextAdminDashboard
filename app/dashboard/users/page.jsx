88d476538221d5321f4b7b3406851368ee39fab6
Ntoi Korotsoane <korotsoanentoi@gmail.com>
2024-11-28 16:03:01 +0200

    commit users data

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -7,7 +7,8 @@ import { fetchUsers } from "../../lib/data";
 
 const Users = async ({ searchParams }) => {
   const q = searchParams?.q || "";
-  const users = await fetchUsers(q);
+  const page = searchParams?.page || 1;
+  const { count, users } = await fetchUsers(q, page);
 
   return (
     <div className={styles.container}>
@@ -63,7 +64,7 @@ const Users = async ({ searchParams }) => {
           ))}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
   );
 };
